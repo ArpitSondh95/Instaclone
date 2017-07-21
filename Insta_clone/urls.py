7a93fc83f66e854889e665ff533a9a045144c39c
Arpit Sondh <arpit_sondh@yahoo.com>
2017-07-21 21:21:34 +0530

    Login and SignUp Page added and hyper linked.

--- Insta_clone/urls.py
+++ Insta_clone/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-
-from myapp.views import signup_view
+from myapp.views import signup_view, login_view
 urlpatterns = [
+    url('login/', login_view),
     url('', signup_view)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,49 @@
 from django.shortcuts import render, redirect
-from forms import SignUpForm
+from forms import SignUpForm, LoginForm
 from models import UserModel
-from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
+from datetime import timedelta
+from django.utils import timezone
 
 # Create your views here.
 
 def signup_view(request):
     if request.method == "POST":
-        print 'Sign up form submitted'
-    elif request.method == 'GET':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            #saving data to DB
+            user = UserModel(name=name, password=make_password(password), email=email, username=username)
+            user.save()
+            return render(request, 'success.html')
+            #return redirect('login/')
+    else:
         form = SignUpForm()
 
     return render(request, 'index.html', {'form' : form})
+
+
+def login_view(request):
+    response_data = {}
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = UserModel.objects.filter(username=username).first()
+
+            if user:
+                # Check for the password
+                if check_password(password, user.password):
+                    print 'User is valid'
+                else:
+                    print 'User is invalid'
+
+    elif request.method == 'GET':
+        form = LoginForm()
+
+    response_data['form'] = form
+    return render(request, 'login.html', response_data)
