a88e0c882987ebccc19487ecd7648232b11dbf1c
Arpit Sondh <arpit_sondh@yahoo.com>
2017-07-24 21:55:13 +0530

    Feed Module added

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from forms import SignUpForm, LoginForm
-from models import UserModel
+from forms import SignUpForm, LoginForm, PostForm
+from models import UserModel, SessionToken, PostModel
 from django.contrib.auth.hashers import make_password, check_password
+from imgurpython import ImgurClient
+from Insta_clone.settings import BASE_DIR
 from datetime import timedelta
 from django.utils import timezone
 
@@ -38,12 +40,58 @@ def login_view(request):
             if user:
                 # Check for the password
                 if check_password(password, user.password):
-                    print 'User is valid'
+                    token = SessionToken(user=user)
+                    token.create_token()
+                    token.save()
+                    response = redirect('feed/')
+                    response.set_cookie(key='session_token', value=token.session_token)
+                    return response
                 else:
-                    print 'User is invalid'
+                    response_data['message'] = 'Incorrect Password! Please try again!'
 
     elif request.method == 'GET':
         form = LoginForm()
 
     response_data['form'] = form
     return render(request, 'login.html', response_data)
+
+
+def feed_view(request):
+    user = check_validation(request)
+    if user:
+        posts = PostModel.objects.all().order_by('created_on')
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+        return redirect('/login/')
+
+
+# For validating the session
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            return session.user
+    else:
+        return None
+
+
+def post_view(request):
+    user = check_validation(request)
+
+    if user:
+        if request.METHOD == 'GET':
+            form = PostForm()
+            return render(request, 'post.html', {'form': form})
+        elif request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data.get('image')
+                caption = form.cleaned_data.get('caption')
+                post = PostModel(user=user, image=image, caption=caption)
+                path = str(BASE_DIR +'\\' + post.image.url)
+                client = ImgurClient("eba875c9581f18e","dc6babb5552228fe1f1909685fb6d8f12aa13edb" )
+                post.image_url = client.upload_from_path(path, anon=True)['link']
+                post.save()
+                return redirect('/feed/')
+        else:
+            return('/login/')
